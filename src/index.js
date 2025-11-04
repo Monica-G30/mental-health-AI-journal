69ce800cb56ef44d3aeae15ca32dafe5c1347dc0
Monica-G30 <wangui16monica@gmail.com>
2025-11-04 13:39:18 +0300

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-function generateResponse(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#response", {
-    strings: [
-      " Holding on to those feelings of warmth can help brighten your day.",
-    ],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateResponse(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2ab48f58932e542cdo049f3057b190t0";
+  let context =
+    "You are a kind, supportive mental health journaling companion.Your role is to listen, reflect, and help users process their emotions in a gentle and non-judgmental way.Never offer medical advice or diagnoses.Encourage mindfulness, gratitude, and self-kindness.Keep your responses under 150 words.End each message with a warm affirmation or gentle reminder.Please sign with SheCodesAI inside a <strong> element at the end of the response and let it be inside a </br> element.";
+  let prompt = `Generate a response about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating response");
+  console.log(`Prompt ${prompt}`);
+  console.log(`Context ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let responseFormElement = document.querySelector("#response-generator-form");
